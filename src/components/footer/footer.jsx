8d7f1ab7ab9e4c65c147fe98c1228445bb170b57
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-10-26 12:39:19 -0400

    style: added temp footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,31 @@
+import { Row, Col } from 'react-bootstrap';
 import '../footer/footer.css';
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div>
+        <Row id='footer'>
+          {/* <Col>
+            <a
+              className='bi bi-instagram'
+              href=''
+              target='_blank'
+              rel='noopener noreferrer'
+            ></a>
+            <a
+              className='bi bi-youtube'
+              href='https://www.youtube.com/@hexlelich'
+              target='_blank'
+              rel='noopener noreferrer'
+            ></a>
+          </Col> */}
+   
+          <div className='copyright'>&copy; HexleLich 2023</div>
+        </Row>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
