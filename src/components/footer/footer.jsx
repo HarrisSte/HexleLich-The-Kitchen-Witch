0d0a8b3305d2b8c3c678937996aeef0154ad88f7
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-11-21 15:19:25 -0500

    refactor: adjusted footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,29 +1,66 @@
+//Importing necessary files for the footer.
+import { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
+
 import '../footer/footer.css';
 
 function Footer() {
+  const [hoveredIcon, setHoveredIcon] = useState(null);
+
+  const handleIconHover = (iconName) => {
+    setHoveredIcon(iconName);
+  };
+
   return (
     <footer>
-      <div>
-        <Row id='footer'>
-          {/* <Col>
-            <a
-              className='bi bi-instagram'
-              href=''
-              target='_blank'
-              rel='noopener noreferrer'
-            ></a>
-            <a
-              className='bi bi-youtube'
-              href='https://www.youtube.com/@hexlelich'
-              target='_blank'
-              rel='noopener noreferrer'
-            ></a>
-          </Col> */}
-   
-          <div className='copyright'>&copy; HexleLich 2023</div>
-        </Row>
-      </div>
+      <Row className='footer'>
+        <Col
+          xs='auto'
+          className={`text-center ${
+            hoveredIcon === 'envelope' ? 'hovered' : ''
+          }`}
+          onMouseEnter={() => handleIconHover('envelope')}
+          onMouseLeave={() => setHoveredIcon(null)}
+        >
+          <a
+            className='bi bi-envelope'
+            href=''
+            target='_blank'
+            rel='noopener noreferrer'
+          ></a>
+        </Col>
+        <Col
+          xs='auto'
+          className={`text-center ${
+            hoveredIcon === 'instagram' ? 'hovered' : ''
+          }`}
+          onMouseEnter={() => handleIconHover('instagram')}
+          onMouseLeave={() => setHoveredIcon(null)}
+        >
+          <a
+            className='bi bi-instagram'
+            href=''
+            target='_blank'
+            rel='noopener noreferrer'
+          ></a>
+        </Col>
+        <Col
+          xs='auto'
+          className={`text-center ${
+            hoveredIcon === 'facebook' ? 'hovered' : ''
+          }`}
+          onMouseEnter={() => handleIconHover('facebook')}
+          onMouseLeave={() => setHoveredIcon(null)}
+        >
+          <a
+            className='bi bi-facebook'
+            href=''
+            target='_blank'
+            rel='noopener noreferrer'
+          ></a>
+        </Col>
+        <div className='copyright'>&copy; Stephanie Harris 2023</div>
+      </Row>
     </footer>
   );
 }
