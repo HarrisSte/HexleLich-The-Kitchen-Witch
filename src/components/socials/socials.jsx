4a706d52e04e1b76f5f3e38a0ce894a95c49e7ba
Stephanie <st3phanie.harris@gmail.com>
2023-11-22 16:54:05 -0500

    bug: links open in new tab

--- src/components/socials/socials.jsx
+++ src/components/socials/socials.jsx
@@ -5,14 +5,24 @@ function Socials() {
   return (
     <Container className='socials-container'>
       <Row>
-        <a href='https://www.youtube.com/@hexlelich' className='button type--A'>
+        <a
+          href='https://www.youtube.com/@hexlelich'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='button type--A'
+        >
           <span className='button__text'>YouTube</span>
           <div className='button__drow1'></div>
           <div className='button__drow2'></div>
         </a>
       </Row>
       <Row>
-        <a href='https://www.twitch.tv/hexlelich' className='button type--A'>
+        <a
+          href='https://www.twitch.tv/hexlelich'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='button type--A'
+        >
           <span className='button__text'>Instagram</span>
           <div className='button__drow1'></div>
           <div className='button__drow2'></div>
@@ -21,6 +31,8 @@ function Socials() {
       <Row>
         <a
           href='https://www.twitch.tv/hexlelich'
+          target='_blank'
+          rel='noopener noreferrer'
           className='button type--A'
         >
           <span className='button__text'>Twitch</span>
