55e27ae27d2f028948ce963a9335784ea97ffd87
Stephanie <st3phanie.harris@gmail.com>
2023-11-25 23:16:30 -0500

    bug: 404 message render issue on deploy

--- src/components/recipes/recipes.jsx
+++ src/components/recipes/recipes.jsx
@@ -1,7 +1,7 @@
 import Image from 'react-bootstrap/Image';
 
 function FluidExample() {
-  return <Image src='../../assets/oops404.png' fluid />;
+  return <Image src='../../assets/oops404.jpg' fluid />;
 }
 
 export default FluidExample;
