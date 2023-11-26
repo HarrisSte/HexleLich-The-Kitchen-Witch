64541b4802962d519a2f0b0cfbf3d64380f15871
Stephanie <st3phanie.harris@gmail.com>
2023-11-25 23:14:02 -0500

    bug: 404 message on unfinished pages

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,7 @@
 import Image from 'react-bootstrap/Image';
 
 function FluidExample() {
-  return <Image src='../../src/assets/oops404.png' fluid />;
+  return <Image src='../../assets/oops404.png' fluid />;
 }
 
 export default FluidExample;
